329762adfacfa77935af3061d5a3b73d1b3de794
hyuk22 <wjdgurdl272@gmail.com>
2019-06-08 09:27:20 +0900

    [Fix] fix semi

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,11 +33,11 @@ exports.createPages = ({ graphql, actions }) => {
         }
 
         // Create blog posts pages.
-        const posts = result.data.allMarkdownRemark.edges;
+        const posts = result.data.allMarkdownRemark.edges
 
         _.each(posts, (post, index) => {
-          const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-          const next = index === 0 ? null : posts[index - 1].node;
+          const previous = index === posts.length - 1 ? null : posts[index + 1].node
+          const next = index === 0 ? null : posts[index - 1].node
 
           createPage({
             path: post.node.fields.slug,
